de181cdaa4bfaa418ddb94fa8be79d002fa498f1
joshuamcguire <joshmcguire@outlook.com.au>
2023-02-11 04:17:49 +1000

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,9 +6,9 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
+          "Bioinformatician",
+          "Full Stack Developer",
+          "React/Svelte/Flutter Specialist",
           "Open Source Contributor",
         ],
         autoStart: true,
